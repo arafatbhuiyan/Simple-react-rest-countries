37b27c66ae7db7c8ca89b7cafcbdd8eb31adeb85
arafatbhuiyan <mdarafatbhuiyan06@gmail.com>
2026-02-06 22:20:54 +0600

    Conditional CSS and Toggle state

--- src/Componenet/Country/country.jsx
+++ src/Componenet/Country/country.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css';
 
 const Country = ({country}) => {
     console.log(country);
+    const [visited , SetVisited] = useState(false)
 
 
    const hendelvisited = ()=>{
-    console.log("button Clicked");
+    if (visited) {
+        SetVisited(false)
+    }
+    else{
+        SetVisited(true)
+    }
     
    }
     
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
 
             <h3>Name :{country.name.common}</h3>
             <p>Population :{country.population.population}</p>
             <p>Area : {country.area.area} {country.area.area > 300000 ? "(Big Country)" :"(Small Country)"}</p>
-            <button onClick={hendelvisited}>Not visited</button>
+            <button onClick={hendelvisited}>{visited ? "Visited": "Not Visited"}</button>
 
 
         </div>
