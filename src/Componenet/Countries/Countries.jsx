ba6072f981ba8eb720aaa06b4ae2665e42e7cc9d
arafatbhuiyan <mdarafatbhuiyan06@gmail.com>
2026-02-06 21:44:16 +0600

    Use style in react and 3 columns layout and conditional text

--- src/Componenet/Countries/Countries.jsx
+++ src/Componenet/Countries/Countries.jsx
@@ -1,20 +1,21 @@
-import React, { use } from 'react';
-import Country from '../Country/country';
+import React, { use } from "react";
+import Country from "../Country/country";
+import "./Countries.css";
 
-const Countries = ({CountriesPromis}) => {
-    const countresData = use(CountriesPromis)
-    const countres = countresData.countries
-    
+const Countries = ({ CountriesPromis }) => {
+  const countresData = use(CountriesPromis);
+  const countres = countresData.countries;
 
-    return (
-        <div>
-            <h1>In the Countries</h1>
-            <h3> Countries : {countres.length}</h3>
-            {
-                countres. map(country => <Country key={country.cca3.cca3} country={country}></Country>)
-            }
-        </div>
-    );
+  return (
+    <div >
+      <h1>In the Countries :{countres.length}</h1>
+      <div className="countries">
+        {countres.map((country) => (
+          <Country key={country.cca3.cca3} country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
--- src/Componenet/Country/country.jsx
+++ src/Componenet/Country/country.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import './Country.css';
 
 const Country = ({country}) => {
     console.log(country);
+
+
+   const hendelvisited = ()=>{
+    console.log("button Clicked");
+    
+   }
     
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
 
             <h3>Name :{country.name.common}</h3>
-            <h3>Population :{country.population.population}</h3>
-
+            <p>Population :{country.population.population}</p>
+            <p>Area : {country.area.area} {country.area.area > 300000 ? "(Big Country)" :"(Small Country)"}</p>
+            <button onClick={hendelvisited}>Not visited</button>
 
 
         </div>
